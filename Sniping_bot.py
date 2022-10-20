3e01af764081f442523efb0d8a109cc8909fb8c1
Zecarad <108441941+Zecarad@users.noreply.github.com>
2022-10-20 00:56:15 -0600

    Subida 2

--- Sniping_bot.py
+++ Sniping_bot.py
@@ -1,5 +1,17 @@
+from time import sleep
+
 from requests_html import HTMLSession
 
 url = "https://extremetechcr.com/tienda/componentes/8036-msi-optix-ag321cqr-32-rgb-2k-165hz-freesync.html"
 
-Sesion = HTMLSession()
+session = HTMLSession()
+
+while True:
+    r: session.get(url)
+    buy_zone = r.html.find("#btnWishAddBuy")
+    if len(buy_zone) > 0:
+        print("Stock disponible")
+    else:
+        print("No hay stock")
+
+    sleep(30)
